e16bb4b44b701bd5ed9f3806c6010e739cd10547
gitmarah <thetelastudio@gmail.com>
2025-12-29 21:15:54 +0000

    Formatted Profile for students

--- src/components/StudentExcerpt.tsx
+++ src/components/StudentExcerpt.tsx
@@ -16,7 +16,7 @@ const StudentExcerpt: React.FC<Props> = ({ student }) => {
             <div className='flex flex-col gap-1'>
                 <div className='flex flex-col gap-2 items-center justify-between'>
                     <div className='flex flex-col items-center gap-2 text-[0.8rem]'>
-                        <img className='w-24 rounded-full' src={student?.profileUrl ?? "/profile.jpg"} />
+                        <img className='w-24 aspect-square object-top object-cover rounded-full' src={student?.profileUrl ?? "/profile.jpg"} />
                         <div className='flex flex-col items-center'>
                             <div className='flex gap-1'>
                                 <p className='font-bold text-sm whitespace-nowrap flex items-center'>{student?.firstname} {student?.lastname}<BadgeCheck size={17} color='#fff' fill='#74B4DA' /></p>
